1042688ca9effc00d9e073800188fdb68184c754
Sebastian <19Sebastian95@gmx.de>
2021-07-01 14:09:45 +0200

    fix dummy test

--- backend/src/test/java/de/hskl/ki/services/ProjectServiceTest.java
+++ backend/src/test/java/de/hskl/ki/services/ProjectServiceTest.java
@@ -2,17 +2,12 @@ package de.hskl.ki.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
 public class ProjectServiceTest {
-
-    @Autowired
-    ProjectService projectService;
 
     @Test
     void moveProjectDirToRoot() {
